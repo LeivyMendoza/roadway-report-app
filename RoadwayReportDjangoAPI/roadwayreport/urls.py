82c06bbfc4ccb936198d0ffa04662e23aae0d823
Leivy Mendoza <mendozaleivy@gmail.com>
2023-11-21 23:31:26 -0500

    add url and views

---
+++ RoadwayReportDjangoAPI/roadwayreport/urls.py
@@ -0,0 +1,20 @@
+"""
+from django.conf.urls import url 
+from tutorials import views 
+ 
+urlpatterns = [ 
+    url(r'^api/tutorials$', views.tutorial_list),
+    url(r'^api/tutorials/(?P<pk>[0-9]+)$', views.tutorial_detail),
+    url(r'^api/tutorials/published$', views.tutorial_list_published)
+]
+"""
+
+from django.urls import path
+from roadwayreport import views
+
+urlpatterns = [
+    path('login/', views.login_view, name='login'),  # For logging in or creating a new user
+    path('dashboard/', views.dashboard_info, name='dashboard_info'),  # For fetching dashboard info
+    path('report/', views.report_pothole, name='report_pothole'),  # For posting a new report
+    path('confirmation/', views.confirmation, name='confirmation'),  # For getting confirmation/status
+]
--- RoadwayReportDjangoAPI/roadwayreport/views.py
+++ RoadwayReportDjangoAPI/roadwayreport/views.py
@@ -1,3 +1,105 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Official, User, Leaderboard, Report, Notification, Media, Comment
+from .serializers import (
+    OfficialSerializer, UserSerializer, LeaderboardSerializer,
+    ReportSerializer, NotificationSerializer, MediaSerializer, CommentSerializer
+)
 
-# Create your views here.
+# Official ViewSet
+class OfficialListCreate(generics.ListCreateAPIView):
+    queryset = Official.objects.all()
+    serializer_class = OfficialSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class OfficialDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Official.objects.all()
+    serializer_class = OfficialSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# User ViewSet
+class UserListCreate(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]  # Allows sign up by anyone
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# Leaderboard ViewSet
+class LeaderboardList(generics.ListAPIView):
+    queryset = Leaderboard.objects.all()
+    serializer_class = LeaderboardSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+# Report ViewSet
+class ReportListCreate(generics.ListCreateAPIView):
+    queryset = Report.objects.all()
+    serializer_class = ReportSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class ReportDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Report.objects.all()
+    serializer_class = ReportSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# Notification ViewSet
+class NotificationListCreate(generics.ListCreateAPIView):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class NotificationDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# Media ViewSet
+class MediaListCreate(generics.ListCreateAPIView):
+    queryset = Media.objects.all()
+    serializer_class = MediaSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class MediaDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Media.objects.all()
+    serializer_class = MediaSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# Comment ViewSet
+class CommentListCreate(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+@api_view(['POST'])
+def login_view(request):
+    pass
+
+@api_view(['GET'])
+def dashboard_info(request):
+    user = request.user
+    reports = Report.objects.filter(user=user)
+    serializer = ReportSerializer(reports, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def report_pothole(request):
+    pass
+
+@api_view(['GET'])
+def confirmation(request):
+    # Pass the report ID as a query parameter.
+    report_id = request.query_params.get('report_id')
+    try:
+        report = Report.objects.get(id=report_id)
+        return Response({'id': report.id, 'status': report.status})
+    except Report.DoesNotExist:
+        return Response({'error': 'Report not found.'}, status=status.HTTP_404_NOT_FOUND)

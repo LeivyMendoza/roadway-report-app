b784ab167448fecbdd8cc7107b79739dbed22c81
Leivy Mendoza <mendozaleivy@gmail.com>
2023-11-25 18:53:42 -0500

    refresh dashboard after submittion of report

--- RoadwayReportAngularClient/src/app/components/dashboard/dashboard.component.ts
+++ RoadwayReportAngularClient/src/app/components/dashboard/dashboard.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { ReportService } from 'src/app/services/report.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ReportPotholeDialogComponent } from '../report-pothole-dialog/report-pothole-dialog.component';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,6 +15,7 @@ export class DashboardComponent implements OnInit {
   reports: any[] = [];
   nonResolvedCount: number = 0;
   displayedColumns: string[] = ['id', 'type', 'status', 'location', 'description', 'createdAt', 'updatedAt'];
+  private destroy$ = new Subject<void>();
 
   constructor(private dialog: MatDialog, private reportService: ReportService) { }
 
@@ -34,9 +37,16 @@ export class DashboardComponent implements OnInit {
     const dialogRef = this.dialog.open(ReportPotholeDialogComponent, {
       width: '600px',
     });
-
-    dialogRef.componentInstance.reportSubmitted.subscribe(() => {
-      this.loadReports();  // Refresh reports when a report is submitted
+  
+    dialogRef.componentInstance.reportSubmitted.pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(() => {
+      this.loadReports(); // Refresh reports when a report is submitted
     });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
